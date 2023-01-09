ad06ef517f0af5a77032c69c5a524cc3da250e56
Michael Bock <no-day@posteo.net>
2023-01-09 13:21:41 +0100

    Remove LocalClass

--- src/LabeledData/VariantLike/LocalClass.purs
+++
@@ -1,8 +0,0 @@
-module LabeledData.VariantLike.LocalClass where
-
-import Data.Variant (Variant)
-
-class VariantLike :: Type -> Type -> Row Type -> Constraint
-class VariantLike tok a r | tok a -> r where
-  toVariant :: tok -> a -> Variant r
-  fromVariant :: tok -> Variant r -> a
